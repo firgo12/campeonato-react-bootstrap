d813d3561c065a804e4c50d4e10a27da7e885d16
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-09-17 17:10:06 -0300

    Updates

---
+++ precache-manifest.02b950d0494b6b4655e49c7077234326.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d59c38b86059e24a1b9b7097010ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02595d9da3837f335d64",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d4340798f7ed91836f00",
+    "url": "/static/css/main.da5be946.chunk.css"
+  },
+  {
+    "revision": "02595d9da3837f335d64",
+    "url": "/static/js/2.205de778.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.205de778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4340798f7ed91836f00",
+    "url": "/static/js/main.9f1b96f2.chunk.js"
+  },
+  {
+    "revision": "9674bbadbc6ad3465c04",
+    "url": "/static/js/runtime-main.ee8c2068.js"
+  }
+]);
