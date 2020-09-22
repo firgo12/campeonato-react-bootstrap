9cdd4845f70b5456be2b6de1f5f47be86e0dc3ac
Giovanni-Perrotta <60896922+Giovanni-Perrotta@users.noreply.github.com>
2020-09-22 15:38:32 -0300

    imagem

--- src/componente1/AboutUs.js
+++ src/componente1/AboutUs.js
@@ -6,18 +6,18 @@ class Nos extends React.Component {
         <>
         <div>
            <h1> Giovanni Perrotta Paulino </h1> 
-        <img src= 'https://instagram.fmgf1-1.fna.fbcdn.net/v/t51.2885-19/s320x320/118309760_310002756750760_4306219943663994447_n.jpg?_nc_ht=instagram.fmgf1-1.fna.fbcdn.net&_nc_ohc=seIAbu6vhZkAX-iRoiQ&oh=9aaaf95dca6b756a9531a373143bb1a9&oe=5F8DFE67'></img>
-        <href class ='texto'> https://github.com/Giovanni-Perrotta</href>
+        <img src= 'https://avatars3.githubusercontent.com/u/60896922?s=400&v=4'></img>
+        <a href="https://github.com/Giovanni-Perrotta" class ='texto' >link text</a>
         </div>
 
         <div>
-           <h1> Guilherme Mendes da Cunha </h1> 
-        <img src= 'https://avatars0.githubusercontent.com/u/60897048?s=460&v=4'></img>
+           <h1> Kauan Victor Ramos dos Santos </h1> 
+        <img src= ''></img>
         </div>
 
         <div>
-           <h1> Juan Victor Angelo de Faria </h1> 
-        <img src= 'https://instagram.fgru5-1.fna.fbcdn.net/v/t51.2885-19/s320x320/119603300_248467123150082_154417975328273756_n.jpg?_nc_ht=instagram.fgru5-1.fna.fbcdn.net&_nc_ohc=MsOnby7JeuQAX90RBuw&oh=ff61e8ad9943394e6ebde31ff70f0b57&oe=5F8DEC36'></img>
+           <h1> João Vitor Pedrozo </h1> 
+        <img src= 'https://avatars1.githubusercontent.com/u/60896892?s=460&v=4'></img>
         </div>
 
         <div>
@@ -26,8 +26,8 @@ class Nos extends React.Component {
         </div>
         
         <div>
-           <h1> Igor Aldivan </h1> 
-        <img src= 'https://avatars0.githubusercontent.com/u/66272426?s=460&v=4'></img>
+           <h1> Victor Morais Bhering </h1> 
+        <img src= 'https://avatars2.githubusercontent.com/u/60671465?s=460&v=4'></img>
         </div>
         
         </>
